43aad12613971a245b0692b52d9dfa3407ed5eba
justojaramillo <justojaramillo1991@gmail.com>
2024-11-03 08:31:41 -0500

    loading efect in nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Results from "@/components/Results";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
@@ -8,8 +9,10 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 } }
   );
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failt to fetch data");
