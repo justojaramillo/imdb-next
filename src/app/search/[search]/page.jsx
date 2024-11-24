0bc0a5c8a9dfa167cbecf8c5aaaec0a3bc9e9ab3
justojaramillo <justojaramillo1991@gmail.com>
2024-11-24 08:50:54 -0500

    search component styled

--- src/app/search/[search]/page.jsx
+++ src/app/search/[search]/page.jsx
@@ -12,10 +12,6 @@ export default async function SearchPage({ params }) {
 
   return (
     <div>
-      {/* {results &&
-        results.length ===
-        <h1 className="text-center pt-6"> No results found</h1>}
-      {results && <Results results={results} />} */}
       {results === 0 ? (
         <h1 className="text-center pt-6"> No results found</h1>
       ) : (
