14379ec8d25216a3a37577a96f6a8c19e097f76c
justojaramillo <justojaramillo1991@gmail.com>
2024-11-24 08:50:14 -0500

    search component styled

---
+++ src/app/search/[search]/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Results from "@/components/Results";
+
+export default async function SearchPage({ params }) {
+  const { search } = params;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${search}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results = data.results;
+  console.log(results);
+
+  return (
+    <div>
+      {/* {results &&
+        results.length ===
+        <h1 className="text-center pt-6"> No results found</h1>}
+      {results && <Results results={results} />} */}
+      {results === 0 ? (
+        <h1 className="text-center pt-6"> No results found</h1>
+      ) : (
+        <Results results={results} />
+      )}
+    </div>
+  );
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Card from "./Card";
 
 export default function Results({ results }) {
